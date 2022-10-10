513d076ccfadc0f22d0707c31169b9d801ea52c7
divo <divoapple@gmail.com>
2022-10-10 17:02:10 +0100

    Finished first skecth
    Topics covered:
    Random seeds
    Polygon paths
    clipping masks
    Color pallets
    Trig
    Shadows

--- first/sketch-skew.js
+++ first/sketch-skew.js
@@ -4,11 +4,17 @@ const random = require('canvas-sketch-util/random');
 const Color = require('canvas-sketch-util/color');
 const risoColors = require('riso-colors');
 
+const seed = random.getRandomSeed();
+
 const settings = {
   dimensions: [ 1080, 1080 ],
+  animate: true,
+  name: seed
 };
 
 const sketch = ({ context, width, height }) => {
+  random.setSeed(seed);
+
   let x, y, w, h, fill, stroke, blend;
 
   const num_shapes = 40;
@@ -22,6 +28,13 @@ const sketch = ({ context, width, height }) => {
   ];
 
   const bgColor = random.pick(risoColors).hex
+
+  const mask = {
+    radius: width * 0.4,
+    sides: 3,
+    x: width * 0.5,
+    y: height * 0.58, // This 0.8 is a hack
+  }
 
   for (let i = 0; i < num_shapes; i++) {
     x = random.range(0, width);
@@ -40,12 +53,22 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
 
+    context.save();
+    context.translate(mask.x, mask.y);
+
+    drawPolygon({ context, radius: mask.radius, sides: mask.sides });
+
+    context.clip();
+
+
     rects.forEach(rect => {
       const { x, y, w, h, fill, stroke, blend } = rect;
       let shadowColor;
 
       context.save();
+      context.translate(-mask.x, -mask.y);
       context.translate(x, y);
+
       context.strokeStyle = stroke;
       context.fillStyle = fill;
       context.lineWidth = 10;
@@ -62,6 +85,7 @@ const sketch = ({ context, width, height }) => {
       context.shadowOffsetY = 20;
 
       context.fill();
+
       context.shadowColor = null;
       context.stroke();
 
@@ -70,7 +94,23 @@ const sketch = ({ context, width, height }) => {
       context.stroke();
 
       context.restore();
-    });
+
+   });
+
+    context.restore();
+
+    // Draw mask outline
+    context.save();
+    context.translate(mask.x, mask.y);
+    context.lineWidth = 30;
+
+    drawPolygon({ context, radius: mask.radius - context.lineWidth + 1, sides: mask.sides });
+
+    context.globalCompositeOperation = 'color-burn';
+    context.strokeStyle = rectColors[0].hex;
+    context.stroke();
+
+    context.restore();
   };
 };
 
@@ -92,5 +132,19 @@ const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
 
   context.restore();
 }
+
+const drawPolygon  = ({ context, radius = 100, sides = 3 }) => {
+  const slice = Math.PI * 2 / sides;
+
+  context.beginPath();
+  context.moveTo(0, -radius);
+
+  for (let i = 1; i < sides; i++) {
+    const theta = i * slice - Math.PI * 0.5;
+    context.lineTo(Math.cos(theta) * radius, Math.sin(theta) * radius);
+  }
+
+  context.closePath();
+};
 
 canvasSketch(sketch, settings);
