35dd467067747c4d8b26703825f13360d9d913b0
divo <divoapple@gmail.com>
2022-10-15 12:02:17 +0100

    lil springy particle

--- particles/sketch-particles.js
+++ particles/sketch-particles.js
@@ -6,9 +6,15 @@ const settings = {
 };
 
 const particles = [];
+const cursor = { x: 9999, y: 9999 };
 
-const sketch = ({ width, height }) => {
+let elCanvas;
+
+const sketch = ({ width, height, canvas }) => {
   let x, y, particle;
+
+  elCanvas = canvas;
+  canvas.addEventListener('mousedown', onMouseDown);
 
   for (let i = 0; i < 1; i ++) {
     x = width * 0.5;
@@ -28,6 +34,30 @@ const sketch = ({ width, height }) => {
     });
   };
 };
+
+onMouseDown = (e) => {
+  window.addEventListener('mousemove', onMouseMove);
+  window.addEventListener('mouseup', onMouseUp);
+
+  onMouseMove(e);
+}
+
+const onMouseMove = (e) => {
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+
+  cursor.x = x;
+  cursor.y = y;
+}
+
+const onMouseUp = () => {
+  window.removeEventListener('mousemove', onMouseMove);
+  window.removeEventListener('mouseup', onMouseUp);
+
+  // Rather than nil-checking just the cursor far away from the so it has no effect
+  cursor.x = 9999;
+  cursor.y = 9999;
+}
 
 canvasSketch(sketch, settings);
 
@@ -50,14 +80,43 @@ class Particle {
     this.iy = y;
 
     this.radius = radius;
+
+    this.minDist = 100;
+    this.pushFactor = 0.02;
+    this.pullFactor = 0.004;
+    this.dampingFactor = 0.95;
   }
 
   update() {
-    this.ax += 0.001;
-    this.ay -= 0.001;
+    let dx, dy, dd, distDelta;
+
+    // pull force
+    dx = this.ix - this.x;
+    dy = this.iy - this.y;
+
+    this.ax = dx * this.pullFactor;
+    this.ay = dy * this.pullFactor;
+
+    // Push force
+    dx = this.x - cursor.x;
+    dy = this.y - cursor.y;
+    dd = Math.sqrt(dx * dx + dy * dy);
+
+    distDelta = this.minDist - dd;
+
+    if (dd < this.minDist) {
+      // We want the accel to be larger the closer the cursor is, in other words
+      // inversly proportional to the distance between particle and cursor
+      // hence the delta
+      this.ax += (dx / dd) * distDelta * this.pushFactor;
+      this.ay += (dy / dd) * distDelta * this.pushFactor;
+    }
 
     this.vx += this.ax;
     this.vy += this.ay;
+
+    this.vx *= this.dampingFactor;
+    this.vy *= this.dampingFactor;
 
     this.x += this.vx;
     this.y += this.vy;
